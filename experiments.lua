8f47f4817e20b381189234f4500cb2a698773406
Matthias Hoelzl <tc@xantira.com>
2012-01-11 04:55:41 +0100

    Changed experiments to their intended duration

--- experiments.lua
+++ experiments.lua
@@ -2,45 +2,45 @@
 experiments = {
    -- 20 robots, small arena
    { file_prefix = "./xml/",
-     substitution_table = build_experiment_table(20, 4, 3600, 123, "libfootbot_foraging.so") 
+     substitution_table = build_experiment_table(20, 4, 36000, 123, "libfootbot_foraging.so") 
    },
    { file_prefix = "./xml/",
-     substitution_table = build_experiment_table(20, 4, 3600, 124, "libfootbot_foraging.so") 
+     substitution_table = build_experiment_table(20, 4, 36000, 124, "libfootbot_foraging.so") 
    },
    { file_prefix = "./xml/",
-     substitution_table = build_experiment_table(20, 4, 3600, 125, "libfootbot_foraging.so") 
+     substitution_table = build_experiment_table(20, 4, 36000, 125, "libfootbot_foraging.so") 
    },
 
    -- 20 robots, big arena
    { file_prefix = "./xml/",
-     substitution_table = build_experiment_table(20, 8, 3600, 123, "libfootbot_foraging.so") 
+     substitution_table = build_experiment_table(20, 8, 36000, 123, "libfootbot_foraging.so") 
    },
    { file_prefix = "./xml/",
-     substitution_table = build_experiment_table(20, 8, 3600, 124, "libfootbot_foraging.so") 
+     substitution_table = build_experiment_table(20, 8, 36000, 124, "libfootbot_foraging.so") 
    },
    { file_prefix = "./xml/",
-     substitution_table = build_experiment_table(20, 8, 3600, 125, "libfootbot_foraging.so") 
+     substitution_table = build_experiment_table(20, 8, 36000, 125, "libfootbot_foraging.so") 
    },
 
    -- 40 robots, small arena
    { file_prefix = "./xml/",
-     substitution_table = build_experiment_table(40, 4, 3600, 123, "libfootbot_foraging.so") 
+     substitution_table = build_experiment_table(40, 4, 36000, 123, "libfootbot_foraging.so") 
    },
    { file_prefix = "./xml/",
-     substitution_table = build_experiment_table(40, 4, 3600, 124, "libfootbot_foraging.so") 
+     substitution_table = build_experiment_table(40, 4, 36000, 124, "libfootbot_foraging.so") 
    },
    { file_prefix = "./xml/",
-     substitution_table = build_experiment_table(40, 4, 3600, 125, "libfootbot_foraging.so") 
+     substitution_table = build_experiment_table(40, 4, 36000, 125, "libfootbot_foraging.so") 
    },
 
    -- 40 robots, big arena
    { file_prefix = "./xml/",
-     substitution_table = build_experiment_table(40, 8, 3600, 123, "libfootbot_foraging.so") 
+     substitution_table = build_experiment_table(40, 8, 36000, 123, "libfootbot_foraging.so") 
    },
    { file_prefix = "./xml/",
-     substitution_table = build_experiment_table(40, 8, 3600, 124, "libfootbot_foraging.so") 
+     substitution_table = build_experiment_table(40, 8, 36000, 124, "libfootbot_foraging.so") 
    },
    { file_prefix = "./xml/",
-     substitution_table = build_experiment_table(40, 8, 3600, 125, "libfootbot_foraging.so") 
+     substitution_table = build_experiment_table(40, 8, 36000, 125, "libfootbot_foraging.so") 
    }
 }
